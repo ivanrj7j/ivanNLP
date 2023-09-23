e4c6a56f177f82da7c3079aab3c4fc5413b1ad36
ivanrj7j <ivanrj7j@gmail.com>
2023-09-23 08:57:31 +0530

    added some new methods

--- ivanNLP/tokenizers/Tokenizer.py
+++ ivanNLP/tokenizers/Tokenizer.py
@@ -1,12 +1,13 @@
 from typing import Union
 from ivanNLP.tokens import Token
+from numpy import ndarray
 
 class Tokenizer:
     """
     Base class for all the tokenizer classes
     """
     def __init__(self) -> None:
-        pass
+        self.vocabulary:list[Token] = []
 
     def fit(self, corpus:Union[str, list[str]]):
         """
@@ -15,9 +16,17 @@ class Tokenizer:
 
         raise NotImplementedError("This method should be implemented by the child")
     
-    def tokenize(self, token:str) -> Token:
+    def getToken(self, token:str) -> Token:
         """
         Gives the token object for the given token
         """
         
         raise NotImplementedError("This method should be implemented by the child")
+    
+    def getTokenFromVector(self, vector:ndarray):
+        results = tuple(filter(lambda x: x.vector == vector, self.vocabulary))
+
+        if len(results) == 0:
+            raise ModuleNotFoundError("There are no token with the given vector")
+        
+        return results[0]
